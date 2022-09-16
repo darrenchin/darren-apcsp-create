6ba8ce64535a4a75236336ec8d07e7c456e7aec7
darrenchin <darrenschoolemail@gmail.com>
2022-09-16 12:30:39 -0400

    array
    checj

---
+++ vite-project/js/array.js
@@ -0,0 +1,64 @@
+const problems = [
+  {
+    problem: "2 + 2",
+    solution: "4",
+  },
+  {
+    problem: "10 x 2",
+    solution: "20",
+  },
+  {
+    problem: "1000-100",
+    solution: "900",
+  },
+  {
+    problem: "100 x 8",
+    solution: "800",
+  },
+  {
+    problem: "100-99",
+    solution: "1",
+  },
+  {
+    problem: "20 + 5",
+    solution: "25",
+  },
+  {
+    problem: "2 x 8",
+    solution: "16",
+  },
+  {
+    problem: "200/4",
+    solution: "50",
+  },
+  {
+    problem: "10 + 2",
+    solution: "12",
+  },
+  {
+    problem: "20 - 2",
+    solution: "4",
+  },
+  {
+    problem: "74/2",
+    solution: "37",
+  },
+  {
+    problem: "50 x 3",
+    solution: "150",
+  },
+  {
+    problem: "29 + 31",
+    solution: "60",
+  },
+  {
+    problem: "1000 - 91",
+    solution: "909",
+  },
+  {
+    problem: "90 x 3",
+    solution: "270",
+  },
+];
+
+problems.forEach((element) => console.log(element));
